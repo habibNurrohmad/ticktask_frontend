0329434fd61ed66a73a1cb73f6e79e2e1a3fc199
Habib Nurrohmad Sugiharto <habib091103@gmail.com>
2025-10-06 01:36:48 +0700

    ndkVersion update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "habibsugiharto.com.ticktask_frontend"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
